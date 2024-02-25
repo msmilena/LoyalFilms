a859394cc3aebb0223574d0dc98c2583f8c9af9a
Milena Mitac <milenamitacsaavedra@gmail.com>
2024-02-25 10:42:51 -0500

    Cambios Footer

--- frontend/my-react-app/src/Pages/HomePage.js
+++ frontend/my-react-app/src/Pages/HomePage.js
@@ -63,7 +63,7 @@ function HomePage() {
           <MainSection />
           <Section sectionName="Tendencias" movies={Tendencias} limit={8}/>
           <Section sectionName="Nuevos" movies={NuevasPeliculas} limit={8}/>
-          <Footer />
+          <Footer showLoginPopup={showLoginPopup} setShowLoginPopup={setShowLoginPopup}/>
         </div>
       </main>
     </div>
