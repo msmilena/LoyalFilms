3f18cb3d32b7ea09bc0ec29c027e07649b95a666
Milena Mitac <milenamitacsaavedra@gmail.com>
2024-03-01 20:21:59 -0500

    Cambios

--- frontend/my-react-app/src/Componentes/NavBar.jsx
+++ frontend/my-react-app/src/Componentes/NavBar.jsx
@@ -1,22 +1,23 @@
-import React, {useState} from 'react';
-import { FaBars, FaTimes } from 'react-icons/fa';
-import { Link as RouterLink,  useNavigate} from 'react-router-dom';
-import { Link as ScrollLink } from 'react-scroll';
-import logo from '../img/Logo.png';
+import React, { useState, useEffect } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { Link as RouterLink, useNavigate } from "react-router-dom";
+import { Link as ScrollLink } from "react-scroll";
+import logo from "../img/Logo.png";
 import { LuUser2 as User } from "react-icons/lu";
 import { IoIosSearch, IoIosArrowDown } from "react-icons/io";
-import './NavBar.css';
+import "./NavBar.css";
 
 const Navbar = ({ showLoginPopup, setShowLoginPopup }) => {
-  const isLoggedIn = !!localStorage.getItem('token');
+  const isLoggedIn = !!localStorage.getItem("token");
 
-  const[click, setClick] = useState(false);
+  const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
   const closeMenu = () => setClick(false);
 
   const handleLogout = () => {
-    localStorage.removeItem('token');
-    window.location.href = '/';
+    localStorage.removeItem("token");
+    localStorage.removeItem("username");
+    window.location.href = "/";
   };
 
   const [searchTerm, setSearchTerm] = useState("");
@@ -27,37 +28,65 @@ const Navbar = ({ showLoginPopup, setShowLoginPopup }) => {
     // Redirigir a la página de resultados de búsqueda con la palabra clave ingresada
     navigate(`/resultados/${searchTerm}`);
   };
+  const [username, setUsername] = useState(""); // Variable de estado para almacenar el nombre del usuario
+  useEffect(() => {
+    setUsername(localStorage.getItem("username"));
+  }, [localStorage.getItem("token")]);
 
   return (
     <nav>
       <img src={logo} alt="Logo" />
       <button className="hamburger" onClick={handleClick}>
-        {click ? (<FaTimes size={30} style={{ color: '#444444' }} />)
-          : (<FaBars size={30} style={{ color: '#444444' }} />)}
+        {click ? (
+          <FaTimes size={30} style={{ color: "#444444" }} />
+        ) : (
+          <FaBars size={30} style={{ color: "#444444" }} />
+        )}
       </button>
       <ul className={click ? "navbar-menu active" : "navbar-menu"}>
         <li>
-          <RouterLink to="/" onClick={closeMenu}>Inicio</RouterLink>
+          <RouterLink to="/" onClick={closeMenu}>
+            Inicio
+          </RouterLink>
         </li>
         <li>
-          <ScrollLink to="Tendencias" onClick={closeMenu}
-          smooth={true} duration={500} spy={true} hashSpy={true}
-          exact='true' offset={50}
-          >Tendencias</ScrollLink>
+          <ScrollLink
+            to="Tendencias"
+            onClick={closeMenu}
+            smooth={true}
+            duration={500}
+            spy={true}
+            hashSpy={true}
+            exact="true"
+            offset={50}
+          >
+            Tendencias
+          </ScrollLink>
         </li>
 
-        <li> 
-        <ScrollLink to="Nuevos" onClick={closeMenu}
-          smooth={true} duration={500} spy={true} hashSpy={true}
-          exact='true' offset={50}
-          >Nuevos</ScrollLink>
+        <li>
+          <ScrollLink
+            to="Nuevos"
+            onClick={closeMenu}
+            smooth={true}
+            duration={500}
+            spy={true}
+            hashSpy={true}
+            exact="true"
+            offset={50}
+          >
+            Nuevos
+          </ScrollLink>
         </li>
 
         <li>
           <div className="dropdown">
-            <div className='contenedorbtn'>
-            <button className="dropbtn">Géneros</button>
-            <IoIosArrowDown className="arrow-icon" style={{marginLeft: '10px'}}/>
+            <div className="contenedorbtn">
+              <button className="dropbtn">Géneros</button>
+              <IoIosArrowDown
+                className="arrow-icon"
+                style={{ marginLeft: "10px" }}
+              />
             </div>
             <div className="dropdown-content">
               <a href="#">Acción</a>
@@ -70,43 +99,58 @@ const Navbar = ({ showLoginPopup, setShowLoginPopup }) => {
         </li>
 
         <li>
-        <div className="search-container">
-          <IoIosSearch className="search-icon" onClick={handleSearch} />
-          <input
-            type="text"
-            placeholder="Buscar una película"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            onKeyDown={(e) => {
-              if (e.key === 'Enter') {
-                handleSearch();
-              }
-            }}
-          />
-        </div>
-
-
+          <div className="search-container">
+            <IoIosSearch className="search-icon" onClick={handleSearch} />
+            <input
+              type="text"
+              placeholder="Buscar una película"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleSearch();
+                }
+              }}
+            />
+          </div>
         </li>
 
         {!isLoggedIn && (
-          <li style={{ display: 'flex', alignItems: 'stretch'}}> 
-          <User size={20} style={{ color: '#C40E61', marginRight: '5px'  }} />
-          <button className="abrirLogin" onClick={() => setShowLoginPopup(true)} style={{color: '#C40E61'}}>Iniciar Sesión</button>
-        </li>
-
+          <li style={{ display: "flex", alignItems: "stretch" }}>
+            <User size={20} style={{ color: "#C40E61", marginRight: "5px" }} />
+            <button
+              className="abrirLogin"
+              onClick={() => setShowLoginPopup(true)}
+              style={{ color: "#C40E61" }}
+            >
+              Iniciar Sesión
+            </button>
+          </li>
         )}
 
         {isLoggedIn && (
-          <li>
-            <RouterLink onClick={handleLogout}>Cerrar Sesión</RouterLink>
-          </li>
+          <ul className="dropdown">
+            <li>
+              <User
+                size={20}
+                style={{ color: "#C40E61", marginRight: "5px" }}
+              />
+              {username}
+            </li>
+
+            <li>
+              <RouterLink onClick={handleLogout}>Cerrar Sesión</RouterLink>
+            </li>
+
+            <li>
+              <RouterLink to="/perfil" onClick={closeMenu}>
+                Perfil
+              </RouterLink>
+            </li>
+          </ul>
         )}
       </ul>
     </nav>
-
-  
-    
-
   );
 };
 
