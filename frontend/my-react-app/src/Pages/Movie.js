9361eb0c29a6db622a2730bc33a0ed37224309ae
Milena Mitac <milenamitacsaavedra@gmail.com>
2024-03-02 03:02:27 -0500

    Cambios

--- frontend/my-react-app/src/Pages/Movie.js
+++ frontend/my-react-app/src/Pages/Movie.js
@@ -1,54 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import Navbar from '../Componentes/NavBar.jsx';
-import { useLocation} from 'react-router-dom';
-import Footer from '../Componentes/Footer.jsx';
-import '../Componentes/Homepage.css';
+import React, { useState, useEffect } from "react";
+import Navbar from "../Componentes/NavBar.jsx";
+import { useLocation } from "react-router-dom";
+import Footer from "../Componentes/Footer.jsx";
+import "../Componentes/Homepage.css";
 function Movie() {
+  const [infoPelicula, setinfoPelicula] = useState([]);
+  const [showLoginPopup, setShowLoginPopup] = useState(false);
+  const location = useLocation();
 
-    const [infoPelicula, setinfoPelicula] = useState([]);
-    const [showLoginPopup, setShowLoginPopup] = useState(false);
-    const location = useLocation();
+  useEffect(() => {
+    const id = new URLSearchParams(location.search).get("id");
+    if (id) {
+      obtenerPelicula(id);
+    } else {
+      alert("No se encontró el ID en la URL");
+    }
+  }, [location.search]);
 
-    useEffect(() => {
-        const id = new URLSearchParams(location.search).get("id");
-        if (id) {
-            obtenerPelicula(id);
-        } else{
-            alert("No se encontró el ID en la URL")
-        }
-    }, [location.search]);
+  function obtenerPelicula(id) {
+    fetch("http://localhost:5000/movies/nuevas", {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((data) => setinfoPelicula(data))
+      .catch((error) => console.error("Error fetching movies:", error));
+  }
 
-    function obtenerPelicula(id) {
-        fetch('http://localhost:5000/movies/nuevas', {
-            method: 'GET',
-        })
-    
-          .then(response => response.json())
-          .then(data => setinfoPelicula(data))
-          .catch(error => console.error('Error fetching movies:', error));
-      }
-
-    return (
-        <div>
-            <header>
-                <Navbar showLoginPopup={showLoginPopup} setShowLoginPopup={setShowLoginPopup} />
-            </header>
-            <main class="mainDiv">
-                <div class="mainMovie">
-                    <div class="infoMovie1">
-                    
-                    </div>
-                    <div class="infoMovie2">
-
-                    </div>
-                </div>
-                <div class="mainResena">
-                </div>
-                <Footer showLoginPopup={showLoginPopup} setShowLoginPopup={setShowLoginPopup} />
-            </main>
-
+  return (
+    <div>
+      <header>
+        <Navbar
+          showLoginPopup={showLoginPopup}
+          setShowLoginPopup={setShowLoginPopup}
+        />
+      </header>
+      <main className="mainDiv">
+        <div className="mainMovie">
+          <div className="infoMovie1"></div>
+          <div className="infoMovie2"></div>
         </div>
-    );
+        <div className="mainResena"></div>
+        <Footer
+          showLoginPopup={showLoginPopup}
+          setShowLoginPopup={setShowLoginPopup}
+        />
+      </main>
+    </div>
+  );
 }
 
 export default Movie;
